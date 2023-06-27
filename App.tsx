4813568c13c4d27aef3adde74d3d78a1d2251716
CharlesCoeder <croederdev@gmail.com>
2023-06-27 11:48:20 -0400

    file picker

--- App.tsx
+++ App.tsx
@@ -2,18 +2,31 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Button from "./src/components/Button";
+import * as DocumentPicker from "expo-document-picker";
 
 export default function App() {
-  const onPress = () => {
-    alert("Button was pressed!");
+  const chooseFileAsync = async () => {
+    let result = await DocumentPicker.getDocumentAsync();
+
+    if (result.type === "success") {
+      alert(`File chosen: ${result.name}`);
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Subtitle Sage</Text>
       <View style={styles.buttonContainer}>
-        <Button label="Select video" color="#ff7a5a" onPress={onPress} />
-        <Button label="Select subtitles" color="#018fac" onPress={onPress} />
+        <Button
+          label="Select video"
+          color="#ff7a5a"
+          onPress={chooseFileAsync}
+        />
+        <Button
+          label="Select subtitles"
+          color="#018fac"
+          onPress={chooseFileAsync}
+        />
       </View>
       <StatusBar style="auto" />
     </View>
