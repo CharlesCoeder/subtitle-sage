cfa01fdf4663f66c0f3f228b59c117e0532c7aab
CharlesCoeder <croederdev@gmail.com>
2023-06-27 11:28:20 -0400

    create Button component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Button from "./src/components/Button";
 
 export default function App() {
+  const onPress = () => {
+    alert("Button was pressed!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header}>Subtitle Sage</Text>
+      <View style={styles.buttonContainer}>
+        <Button label="Select video" color="#ff7a5a" onPress={onPress} />
+        <Button label="Select subtitles" color="#018fac" onPress={onPress} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +27,12 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  buttonContainer: {},
 });
