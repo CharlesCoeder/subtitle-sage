6807d0e85a8f0c2591adfd0992736b61cd5f1165
CharlesCoeder <croederdev@gmail.com>
2023-09-24 15:19:26 -0400

    Import dictionaries via file picker

--- database/src/App.js
+++ database/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Q } from '@nozbe/watermelondb'
 import kuromoji from 'kuromoji';
 
-function App({database}) {
+function App({database, handleClick}) {
   const [term, setTerm] = useState("");
   const [tokenizeTerm, setTokenizeTerm] = useState("");
 
@@ -17,10 +17,10 @@ function App({database}) {
 
   const searchInput = useRef(null);
   const tokenizeSearchInput = useRef(null);
-  
-  async function handleSearch() {
+
+  async function searchForTerm(term){
     const start = performance.now();
-    const matches = await database.collections.get('termBank').query(Q.where('termText', searchInput.current.value)).fetch();
+    const matches = await database.collections.get('termBank').query(Q.where('termText', term)).fetch();
     console.log('\n\n-----------------------------------------')
     console.log('------ SEARCHING FOR ', searchInput.current.value, ' -------')
     console.log('-----------------------------------------')
@@ -39,65 +39,35 @@ function App({database}) {
         console.error('Error with definitions', error);
       }
     });
-    
-    
-  
+
     const end = performance.now();
     const timeTaken = end - start;
     console.log(`Search took ${timeTaken} ms`);
   }
+  
+  async function handleSearch() {
+    searchForTerm(searchInput.current.value);
+  }
 
   async function handleTokenizeSearch(){
-
     const sentence = tokenizeSearchInput.current.value;
 
     kuromoji.builder({ dicPath: "/dict/" }).build(async (err, tokenizer) => {
-      if (err) {
-        throw err;
-      }
-    
-      // Tokenize the sentence
-      const tokens = tokenizer.tokenize(sentence);
-    
-      // Log the tokens
-      console.log(tokens);
+      if (err) {throw err;}
 
+      // tokenize sentence
+      const tokens = tokenizer.tokenize(sentence);
+
+      // string that will show separated tokens in a readable way
       let separated = "";
 
-    
       for (const x of tokens){
         const term = x.basic_form;
-
         separated += " | " + term;
-
-        const matches = await database.collections.get('termBank').query(Q.where('termText', term)).fetch();
-        console.log('\n\n-----------------------------------------')
-        console.log('------ SEARCHING FOR ', term, ' -------')
-        console.log('-----------------------------------------')
-
-  
-        matches.forEach(match => {
-          try {
-            let definitions = match.definitions;
-            if(Array.isArray(definitions)) {
-              definitions.forEach(def => console.log(def));
-            } else {
-              console.error('Definitions are not in expected format');
-              console.log(typeof definitions);
-            }
-          } 
-          
-          catch (error) {
-            console.error('Error with definitions', error);
-          }
-        });
-      }
+        searchForTerm(term);}
       console.log(separated);
     })
-  }
-  
-  
-  
+  }  
 
   return (
     <div className="App">
@@ -111,6 +81,7 @@ function App({database}) {
         <p>
           *Open console to see output*
         </p>
+        <button onClick={handleClick}>Import Dictionary</button>
       </header>
     </div>
   );
--- database/src/index.js
+++ database/src/index.js
@@ -6,9 +6,7 @@ import {Database, Q} from '@nozbe/watermelondb';
 import LokiJSAdapter from '@nozbe/watermelondb/adapters/lokijs';
 import schema from './model/schema';
 import termBank from './model/termBank';
-
-import kuromoji from 'kuromoji'
-// import Zlib from "zlibjs";
+import JSZip from 'jszip';
 
 // WatermelonDB setup
 const adapter = new LokiJSAdapter({
@@ -22,24 +20,76 @@ const adapter = new LokiJSAdapter({
     modelClasses: [termBank],
     actionsEnabled: true,
   });
+  
+  // This function will open a file selector to unzip & import a dictionary
+  const handleClick = async () => {
+    console.log('clicked');
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.zip'; // Only accept .zip files
+  
+    input.addEventListener('change', async (event) => {
+      const file = event.target.files[0]; // Get the selected file
+  
+      if (file) {
+        // Create a FileReader object to read the file
+        const reader = new FileReader();
+        
+        // Add an event listener to handle file read
+        reader.addEventListener('load', async () => {
+          const zip = new JSZip();
+          const contents = await zip.loadAsync(reader.result);
+          const fileArray = [];
+  
+          // Iterate through each file and perform actions
+          for (const filename of Object.keys(contents.files)) {
+            if (!contents.files[filename].dir) { // Skip directories
+              const fileData = await contents.file(filename).async('string'); // Use 'uint8array' for binary files
+              console.log(`File: ${filename}`);
+              fileArray.push({ filename, content: fileData });
+            }
+          }
+  
+          // Now we have a fileArray that can be used for import
+          if (fileArray.length > 0) {
+            // Here, call your importDictionary function
+            await importDictionary(fileArray);
+          }
+        });
+        
+        // Read the file as ArrayBuffer
+        reader.readAsArrayBuffer(file);
+      }
+    });
+  
+    input.click();
+  };
 
 
-  // import function
-  async function importDictionary(dictionaryFolderPath) {
+  // Dictionary import function
+  // fileArray is an array of { fileName, content } for every file in the zipped dictionary archive
+  async function importDictionary(fileArray) {
     console.log("Starting import...");
-    let i = 1;
     const termBankCollection = database.collections.get('termBank');
   
-    while (true) {
-      const filePath = `${dictionaryFolderPath}/term_bank_${i}.json`;
-      const fileContentsResponse = await fetch(filePath);
+    for (const fileObj of fileArray) {
+      const { filename, content } = fileObj;
   
-      if (!fileContentsResponse.ok || !fileContentsResponse.headers.get("content-type").includes("application/json")) {
-        break;
+      // Check if it's a JSON file and matches the filename pattern
+      if (!filename.includes('.json') || !filename.startsWith('term_bank_')) {
+        console.log(`Skipping file ${filename} as it doesn't match the expected pattern.`);
+        continue;
       }
   
-      const entries = await fileContentsResponse.json();
-      console.log(i);
+      let entries;
+      try {
+        entries = JSON.parse(content);
+      } catch (e) {
+        console.error(`Error parsing JSON from file ${filename}: ${e}`);
+        continue;
+      }
+  
+      console.log(`Processing ${filename}...`);
   
       for (const entry of entries) {
         await database.write(async () => {
@@ -63,34 +113,17 @@ const adapter = new LokiJSAdapter({
             );
             console.log(`Data written for termText: ${entry[0]} and sequenceNum: ${entry[6]}. Definitions: ${entry[5]}`);
           } else {
-            // console.log(`Entry with termText: ${entry[0]} and reading: ${entry[1]} already exists. Skipping.`);
+            // Existing entry, do nothing
           }
-          // console.log(entry[5]);
-          // console.log(typeof entry[5]);
         });
       }
-  
-      i++;
     }
     console.log("Import completed");
   }
-  
-
-
-async function start() {
-  importDictionary("/jmdict_english").then(() => {
-    console.log('hey')
-  })
-}
-
-// uncomment to import test dictionary
-// start();
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App database={database}/>
+    <App database={database} handleClick={handleClick}/>
   </React.StrictMode>
 );
